061215c04e1015ff711b44a7aa6a7b19442e7945
Hayden Young <hayden.young@espprojects.co.uk>
2022-08-30 12:13:22 +0100

    feat: implement gif endpoint

---
+++ pages/api/gif/[id].ts
@@ -0,0 +1,16 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import axios from 'axios';
+import sharp from "sharp";
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    const { id } = req.query;
+    const webpUrl = `https://cdn.7tv.app/emote/${id}/3x`;
+
+    const webp = await axios.get(webpUrl, { responseType: 'arraybuffer' });
+    const buffer = Buffer.from(webp.data, "utf-8");
+
+    const s = await sharp(buffer, { pages: -1 }).gif().toBuffer();
+
+    res.end(s);
+}
+
